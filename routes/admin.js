24cd29c61e621d36c94aeb3d47d2fcb8a0406545
hoangvunguyenphi <hoangvunguyenphi@gmail.com>
2018-11-04 01:14:35 +0700

    remove

--- routes/admin.js
+++
@@ -1,6 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var Order_controller = require("../controller/order_controller");
-var Book_controller = require("../controller/book_controller");
-
-module.exports = router;
--- routes/order.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var OrderController = require('../controller/order_controller');
-router.get("/", OrderController.getAllOrder);
-router.get("/detail/:id", OrderController.getDetailOrder);
-
-module.exports = router;
